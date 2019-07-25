8815a0ab295799ad4ea4ce57469b1e6e982bca72
Maxtanie Petit Dol <maxtaniepetitdol@outlook.fr>
2019-07-25 19:59:53 +0200

    Changed

--- server.js
+++ server.js
@@ -66,58 +66,58 @@ app.use("/", teachings.router);
 app.use("/admin", admin);
 
 // Create our Movies Model
-console.log(movies);
-console.log("--------");
-pushSomeData = () => {
-  MoviesModel.insertMany(movies)
-    .then(dbRes => {
-      console.log(dbRes);
-      dbRes
-        .forEach((Movies, i) => console.log(Movies.title, i))
-        .catch(dbErr => {
-          console.log(dbErr);
-        });
-    })
-    .catch(dbErr => {
-      console.log(dbErr);
-    });
-};
-pushSomeData();
+// console.log(movies);
+// console.log("--------");
+// pushSomeData = () => {
+//   MoviesModel.insertMany(movies)
+//     .then(dbRes => {
+//       console.log(dbRes);
+//       dbRes
+//         .forEach((Movies, i) => console.log(Movies.title, i))
+//         .catch(dbErr => {
+//           console.log(dbErr);
+//         });
+//     })
+//     .catch(dbErr => {
+//       console.log(dbErr);
+//     });
+// };
+// pushSomeData();
 
-pushTeachingsData = () => {
-  TeachingsAdultsModel.insertMany(teachingsAdults)
-    .then(dbRes => {
-      console.log(dbRes);
-      dbRes
-        .forEach((teachingsAdults, i) => console.log(teachingsAdults.text, i))
-        .catch(dbErr => {
-          console.log(dbErr);
-        });
-    })
-    .catch(dbErr => {
-      console.log(dbErr);
-    });
-};
-pushTeachingsData();
+// pushTeachingsData = () => {
+//   TeachingsAdultsModel.insertMany(teachingsAdults)
+//     .then(dbRes => {
+//       console.log(dbRes);
+//       dbRes
+//         .forEach((teachingsAdults, i) => console.log(teachingsAdults.text, i))
+//         .catch(dbErr => {
+//           console.log(dbErr);
+//         });
+//     })
+//     .catch(dbErr => {
+//       console.log(dbErr);
+//     });
+// };
+// pushTeachingsData();
 
-console.log(admin);
-console.log("--------");
+// console.log(admin);
+// console.log("--------");
 
-pushSomeAdminData = () => {
-  AdminModel.insertMany(Admin)
-    .then(dbRes => {
-      console.log(dbRes);
-      dbRes
-        .forEach((Admin, i) => console.log(Admin.username, i))
-        .catch(dbErr => {
-          console.log(dbErr);
-        });
-    })
-    .catch(dbErr => {
-      console.log(dbErr);
-    });
-};
-pushSomeAdminData();
+// pushSomeAdminData = () => {
+//   AdminModel.insertMany(Admin)
+//     .then(dbRes => {
+//       console.log(dbRes);
+//       dbRes
+//         .forEach((Admin, i) => console.log(Admin.username, i))
+//         .catch(dbErr => {
+//           console.log(dbErr);
+//         });
+//     })
+//     .catch(dbErr => {
+//       console.log(dbErr);
+//     });
+// };
+// pushSomeAdminData();
 
 app.listen(process.env.PORT, () => {
   console.log("App hosted on: ", process.env.SITE_URL);
